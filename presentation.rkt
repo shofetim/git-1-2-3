0e594c4c116107359cafe08acbe62073abce7d4b
Jordan Schatz <jordan@noionlabs.com>
2011-07-04 20:17:16 -0600

    WIP presentation

--- presentation.rkt
+++ presentation.rkt
@@ -10,9 +10,9 @@
  (para #:align 'center
        "A fast introduction to git for professionals")
  (blank)
- (para  #:align 'center
-        "Jordan Schatz" 
-        (text "jordan@noionlabs.com" '(bold . modern) 25))
+ (para #:align 'center
+       "Jordan Schatz"
+       (text "jordan@noionlabs.com" '(bold . modern) 25))
  (blank)
  (para #:align 'center
        "You can fork this presentation at:"
@@ -29,11 +29,11 @@
  (para
   "Git is:"
   (plus
-        "a" (bit "source code management") "tool")
+   "a" (bit "source code management") "tool")
   (plus
-        "distributed")
+   "distributed")
   (plus
-        "fast")))
+   "fast")))
 
 (slide
  #:title "What is Git?"
@@ -52,14 +52,14 @@
  (para "Why the name 'git' ? ")
  (blank)
  (para
-  "Quoting Linus: \"I'm an egotistical ***, and I name all my projects" 
+  "Quoting Linus: \"I'm an egotistical ***, and I name all my projects"
   "after myself. First 'Linux', now 'git'\""))
 
 (slide
  #:title "What is Git good for?"
  #:layout 'top
- (para "Git is a " 
-       (bit "Source Code Management") 
+ (para "Git is a "
+       (bit "Source Code Management")
        "system, not a version control system.")
  'next
  (plus "Efficient Sharing")
@@ -84,7 +84,7 @@
        (colorize (bit "Experimentation Is Inexpensive") "green")))
 (slide
  (para #:align 'center
-  "Stop pussyfooting around your codebase... and start striding around like a giant"))
+       "Stop pussyfooting around your codebase... and start striding around like a giant"))
 
 (slide
  #:title "Aside"
@@ -122,16 +122,16 @@
  #:title "Where to get it?"
  (t "Git http://git-scm.com/")
  (hc-append 20
-  (t "Linux")
-  (bitmap "./images/tux.png"))
+            (t "Linux")
+            (bitmap "./images/tux.png"))
  (blank)
  (hc-append 20
-  (t "Mac")
-  (bitmap "./images/osx.png"))
+            (t "Mac")
+            (bitmap "./images/osx.png"))
  (blank)
  (hc-append 20
-  (t "Windows")
-  (bitmap "./images/windows.png")))
+            (t "Windows")
+            (bitmap "./images/windows.png")))
 
 (slide
  #:title "How it works"
@@ -154,193 +154,299 @@
  (t "3: Try it out"))
 
 (slide
- #:title "git-init"
- (para (tt "git-init")
-       "Create an empty git repository or reinitialize an existing one."))
-
-(slide   
- #:title "git-clone"
- (para (tt "git-clone")
-       "Clone a repository into a new directory."))
-
-(slide   
- #:title "git-add"
- (para (tt "git-add")
-       "Add file contents to the index."))
-
-(slide   
- #:title "git-commit"
- (para (tt "git-commit")
-       "Record changes to the repository."))
-
-(slide   
- #:title "git-diff"
- (para (tt "git-diff")
-       "Show changes between commits, commit and working tree, etc."))
-
-(slide   
- #:title "git-log"
- (para (tt "git-log")
-       "Show commit logs."))
-
-(slide   
- #:title "git-reset"
- (para (tt "git-reset")
-       "Reset current HEAD to the specified state."))
-
-(slide   
- #:title "git-status"
- (para (tt "git-status")
-       "Show the working tree status."))
-
-(slide   
- #:title "git-show"
- (para (tt "git-show")
-       "Show various types of objects."))
-
-(slide   
- #:title "git-stash"
- (para (tt "git-stash")
-       "Stash the changes in a dirty working directory away."))
-
-(slide   
- #:title "git-pull"
- (para (tt "git-pull")
-       "Fetch from and merge with another repository or a local branch."))
-
-(slide   
- #:title "git-push"
- (para (tt "git-push")
-       "Update remote refs along with associated objects."))
-
-(slide   
- #:title "git-fetch"
- (para (tt "git-fetch")
-       "Download objects and refs from another repository."))
-
-(slide   
- #:title "git-branch"
- (para (tt "git-branch")
-       "List, create, or delete branches."))
-
-(slide   
- #:title "git-checkout"
- (para (tt "git-checkout")
-       "Checkout a branch or paths to the working tree."))
-
-(slide   
- #:title "git-rm"
- (para (tt "git-rm")
-       "Remove files from the working tree and from the index."))
-
-(slide   
- #:title "git-tag"
- (para (tt "git-tag")
-       "Create, list, delete or verify a tag object signed with GPG."))
-
-(slide   
- #:title "gitk"
- (para (tt "gitk")
-       "The git repository browser."))
-
-(slide   
- #:title "git-config"
- (para (tt "git-config")
-       "Get and set repository or global options."))
-
-(slide   
- #:title "git-remote"
- (para (tt "git-remote")
-       "manage set of tracked repositories."))
-
-(slide   
- #:title "git-grep"
- (para (tt "git-grep")
-       "Print lines matching a pattern."))
-
-(slide   
- #:title "git-gui"
- (para (tt "git-gui")
-       "A portable graphical interface to Git."))
-
-(slide   
- #:title "git-clean"
- (para (tt "git-clean")
-       "Remove untracked files from the working tree."))
-
-(slide   
- #:title "git-fsck"
- (para (tt "git-fsck")
-       "Verifies the connectivity and validity of the objects in the database."))
-
-(slide   
- #:title "git-gc"
- (para (tt "git-gc")
-       "Cleanup unnecessary files and optimize the local repository."))
-
-(slide   
- #:title "git-prune"
- (para (tt "git-prune")
-       "Prune all unreachable objects from the object database."))
-
-(slide   
- #:title "git-merge"
- (para (tt "git-merge")
-       "Join two or more development histories together."))
-
-(slide   
- #:title "more advanced commands"
- (para "--- more advanced commands ---"))
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-init")
+  (para "Create an empty git repository.")))
 
 (slide
- #:title "git-rebase"
- (para (tt "git-rebase")
-       "Forward-port local commits to the updated upstream head."))
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-clone")
+  (blank)
+  (t "Clone a repository into a new directory.")))
 
-(slide   
- #:title "git-cherry-pick"
- (para (tt "git-cherry-pick")
-       "Apply the changes introduced by some existing commits."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-add")
+  (blank)
+  (t "Add file contents to the index.")))
 
-(slide   
- #:title "git-bisect"
- (para (tt "git-bisect")
-       "Find by binary search the change that introduced a bug."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-commit")
+  (blank)
+  (t "Record changes to the repository.")))
 
-(slide   
- #:title "git-format-patch"
- (para (tt "git-format-patch")
-       "Prepare patches for e-mail submission."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-diff")
+  (blank)
+  (t "Show changes between commits, commit and working tree, etc.")))
 
-(slide   
- #:title "git-am"
- (para (tt "git-am")
-       "Apply a series of patches from a mailbox."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-log")
+  (blank)
+  (t "Show commit logs.")))
 
-(slide   
- #:title "git-blame"
- (para (tt "git-blame")
-       "Show what revision and author last modified each line of a file."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-reset")
+  (blank)
+  (t "Reset current HEAD to the specified state.")))
 
-(slide   
- #:title "git-archive"
- (para (tt "git-archive")
-       "Create an archive of files from a named tree."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-status")
+  (blank)
+  (t "Show the working tree status.")))
 
-(slide   
- #:title "git-mv"
- (para (tt "git-mv")
-       "Move or rename a file, a directory, or a symlink."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-show")
+  (blank)
+  (t "Show various types of objects.")))
 
-(slide   
- #:title "git-revert"
- (para (tt "git-revert")
-       "Revert some existing commits."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-stash")
+  (blank)
+  (t "Stash the changes in a dirty working directory away.")))
 
-(slide   
- #:title "git-shortlog"
- (para (tt "git-shortlog")
-       "Summarize git log output."))
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-pull")
+  (blank)
+  (t "Fetch from and merge with another repository or a local branch.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-push")
+  (blank)
+  (t "Update remote refs along with associated objects.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-fetch")
+  (blank)
+  (t "Download objects and refs from another repository.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-branch")
+  (blank)
+  (t "List, create, or delete branches.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-checkout")
+  (blank)
+  (t "Checkout a branch or paths to the working tree.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-rm")
+  (blank)
+  (t "Remove files from the working tree and from the index.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-tag")
+  (blank)
+  (t "Create, list, delete or verify a tag object signed with GPG.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "gitk")
+  (blank)
+  (t "The git repository browser.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-config")
+  (blank)
+  (t "Get and set repository or global options.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-remote")
+  (blank)
+  (t "manage set of tracked repositories.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-grep")
+  (blank)
+  (t "Print lines matching a pattern.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-gui")
+  (blank)
+  (t "A portable graphical interface to Git.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-clean")
+  (blank)
+  (t "Remove untracked files from the working tree.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-fsck")
+  (blank)
+  (t "Verifies the connectivity and validity of the objects in the database.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-gc")
+  (blank)
+  (t "Cleanup unnecessary files and optimize the local repository.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-prune")
+  (blank)
+  (t "Prune all unreachable objects from the object database.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-merge")
+  (blank)
+  (t "Join two or more development histories together.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-rebase")
+  (blank)
+  (t "Forward-port local commits to the updated upstream head.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-cherry-pick")
+  (blank)
+  (t "Apply the changes introduced by some existing commits.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-bisect")
+  (blank)
+  (t "Find by binary search the change that introduced a bug.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-format-patch")
+  (blank)
+  (t "Prepare patches for e-mail submission.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-am")
+  (blank)
+  (t "Apply a series of patches from a mailbox.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-blame")
+  (blank)
+  (t "Show what revision and author last modified each line of a file.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-archive")
+  (blank)
+  (t "Create an archive of files from a named tree.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-mv")
+  (blank)
+  (t "Move or rename a file, a directory, or a symlink.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-revert")
+  (blank)
+  (t "Revert some existing commits.")))
+
+(slide
+ #:title "Git Commands"
+ #:layout 'top
+ (para
+  (tt "git-shortlog")
+  (blank)
+  (t "Summarize git log output.")))
 
 (slide
  (t "Addendum"))
@@ -348,16 +454,23 @@
 (slide
  #:title "Gotcha's"
  (plus "Git tracks content, not files. ")
+ 'next
  (plus ".gitignore")
+ 'next
  (plus "git push (it does the right thing but)")
+ 'next
  (plus "making commits/checkouts as root")
+ 'next
  (plus "chmod'ing the hooks when you didn't mean too..."))
 
 (slide
  #:title "Tools "
  (plus "Github https://github.com/")
+ 'next
  (plus "Deploy HQ http://www.deployhq.com/")
+ 'next
  (plus "Gource http://code.google.com/p/gource/")
+ 'next
  (plus "Gitosis")
  (para "http://scie.nti.st/2007/11/14/"
        "hosting-git-repositories-the-easy-and-secure-way"))
